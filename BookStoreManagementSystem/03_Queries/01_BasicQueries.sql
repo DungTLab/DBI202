e746c094550218a0503b1da843da56f01f333da1
giacthce200291-stack <giacthce200291@gmail.com>
2026-02-13 16:37:40 +0700

    Add basic queries for ORDER_DETAIL table

---
+++ BookStoreManagementSystem/03_Queries/01_BasicQueries.sql
@@ -0,0 +1,21 @@
+/* Order_Detail quetry - Giac*/
+
+/*BASIC QUERY*/
+
+-- Select all:
+SELECT *
+FROM ORDER_DETAIL;
+
+-- Select with quantity greater than 1:
+SELECT *
+FROM ORDER_DETAIL
+WHERE quantity > 1;
+
+-- Select with price greater than 20:
+SELECT *
+FROM ORDER_DETAIL
+WHERE price > 20
+
+-- Calculate total for each lines:
+SELECT order_id, book_id, quantity, price, quantity * price AS total
+FROM ORDER_DETAIL;
